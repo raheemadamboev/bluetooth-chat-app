4d7237fb21f8d62b4b37e7fc18af2dc39cb6f052
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-05-29 15:33:37 +0500

    location required fragment
    Took 9 minutes

--- app/src/main/java/xyz/teamgravity/bluetoothchat/fragment/LocationRequiredFragment.kt
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/fragment/LocationRequiredFragment.kt
@@ -1,11 +1,19 @@
 package xyz.teamgravity.bluetoothchat.fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import xyz.teamgravity.bluetoothchat.databinding.FragmentLocationRequiredBinding
+import xyz.teamgravity.bluetoothchat.helper.extension.log
+
+private const val TAG = "LocationRequiredFrag:"
+private const val LOCATION_REQUEST_CODE = 0
 
 class LocationRequiredFragment : Fragment() {
 
@@ -14,7 +22,55 @@ class LocationRequiredFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentLocationRequiredBinding.inflate(inflater, container, false)
+
+        // setup click listener on grant permission button
+        binding.grantPermissionButton.setOnClickListener {
+            checkLocationPermission()
+        }
+
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // check location permission when the Fragment becomes visible on screen
+        checkLocationPermission()
+    }
+
+    private fun showError() {
+        binding.locationErrorMessage.visibility = View.VISIBLE
+        binding.grantPermissionButton.visibility = View.VISIBLE
+    }
+
+    private fun checkLocationPermission() {
+        val hasLocationPermission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (hasLocationPermission) {
+            // Navigate to the chat fragment
+            findNavController().navigate(LocationRequiredFragmentDirections.actionStartChat())
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        log(TAG, "onRequestPermissionsResult: ")
+        when (requestCode) {
+            LOCATION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // Navigate to the chat fragment
+                    findNavController().navigate(LocationRequiredFragmentDirections.actionStartChat())
+                } else {
+                    showError()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
