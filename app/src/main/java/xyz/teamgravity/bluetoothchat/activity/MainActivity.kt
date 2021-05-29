09cca4076c559529fbb6907b6157e7c769bc14a1
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-05-29 18:00:50 +0500

    main activity
    Took 1 minute

--- app/src/main/java/xyz/teamgravity/bluetoothchat/activity/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/activity/MainActivity.kt
@@ -3,10 +3,22 @@ package xyz.teamgravity.bluetoothchat.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import xyz.teamgravity.bluetoothchat.R
+import xyz.teamgravity.bluetoothchat.helper.server.ChatServer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Run the chat server as long as the app is on screen
+    override fun onStart() {
+        super.onStart()
+        ChatServer.startServer(application)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        ChatServer.stopServer()
     }
 }
