d546dbda108af6120507ac5de6c1aca46e8a9773
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-05-30 12:36:49 +0500

    android 10 permissions
    Took 5 minutes

--- app/src/main/java/xyz/teamgravity/bluetoothchat/fragment/LocationRequiredFragment.kt
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/fragment/LocationRequiredFragment.kt
@@ -2,20 +2,31 @@ package xyz.teamgravity.bluetoothchat.fragment
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import xyz.teamgravity.bluetoothchat.databinding.FragmentLocationRequiredBinding
 import xyz.teamgravity.bluetoothchat.helper.extension.log
 
-private const val TAG = "LocationRequiredFrag:"
-private const val LOCATION_REQUEST_CODE = 0
-
 class LocationRequiredFragment : Fragment() {
+    companion object {
+        private const val TAG = "LocationRequiredFrag:"
+        private const val PERMISSIONS_REQUEST_CODE = 1
+        private val PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        @RequiresApi(Build.VERSION_CODES.Q)
+        private val PERMISSIONS_Q = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        )
+    }
 
     private var _binding: FragmentLocationRequiredBinding? = null
     private val binding get() = _binding!!
@@ -42,17 +53,33 @@ class LocationRequiredFragment : Fragment() {
         binding.grantPermissionButton.visibility = View.VISIBLE
     }
 
-    private fun checkLocationPermission() {
-        val hasLocationPermission = ContextCompat.checkSelfPermission(
-            requireContext(),
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
+    // check permissions
+    private fun hasPermissions(): Boolean {
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            PERMISSIONS.all {
+                ActivityCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+            }
+        } else {
+            PERMISSIONS_Q.all {
+                ActivityCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+            }
+        }
+    }
 
-        if (hasLocationPermission) {
-            // Navigate to the chat fragment
+    // request permissions
+    private fun requestPermissions() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            ActivityCompat.requestPermissions(requireActivity(), PERMISSIONS, PERMISSIONS_REQUEST_CODE)
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), PERMISSIONS_Q, PERMISSIONS_REQUEST_CODE)
+        }
+    }
+
+    private fun checkLocationPermission() {
+        if (hasPermissions()) {
             findNavController().navigate(LocationRequiredFragmentDirections.actionStartChat())
         } else {
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+            requestPermissions()
         }
     }
 
@@ -60,11 +87,10 @@ class LocationRequiredFragment : Fragment() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         log(TAG, "onRequestPermissionsResult: ")
         when (requestCode) {
-            LOCATION_REQUEST_CODE -> {
+            PERMISSIONS_REQUEST_CODE -> {
                 if (grantResults.isNotEmpty() &&
                     grantResults[0] == PackageManager.PERMISSION_GRANTED
                 ) {
-                    // Navigate to the chat fragment
                     findNavController().navigate(LocationRequiredFragmentDirections.actionStartChat())
                 } else {
                     showError()
